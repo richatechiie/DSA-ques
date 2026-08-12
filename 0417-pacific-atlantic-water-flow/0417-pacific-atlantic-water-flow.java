7c4a952f14ae74b64ebfb890ff3672283ad138f8
richatechiie <156651781+richatechiie@users.noreply.github.com>
2026-08-12 21:03:17 +0530

    Time: 5 ms (53.06%), Space: 47.5 MB (58.77%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int m = heights.length, n = heights[0].length;
+
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(heights, pacific, 0, j);
+            dfs(heights, atlantic, m - 1, j);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int[][] heights, boolean[][] visited, int r, int c) {
+        if (visited[r][c]) return;
+
+        visited[r][c] = true;
+
+        int[][] dirs = {
+            {1, 0}, {-1, 0}, {0, 1}, {0, -1}
+        };
+
+        for (int[] d : dirs) {
+            int nr = r + d[0];
+            int nc = c + d[1];
+
+            if (nr >= 0 && nr < heights.length &&
+                nc >= 0 && nc < heights[0].length &&
+                !visited[nr][nc] &&
+                heights[nr][nc] >= heights[r][c]) {
+
+                dfs(heights, visited, nr, nc);
+            }
+        }
+    }
+}
